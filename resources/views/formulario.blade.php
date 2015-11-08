4dde350aef7a86a9fafb10f0c7c3524c73e1b626
Luiz <luizz_andrade@hotmail.com>
2015-11-08 19:25:21 -0400

    tt

---
+++ resources/views/formulario.blade.php
@@ -0,0 +1,39 @@
+@extends('template.template')
+
+@section('content')
+
+    <h1>Fórmulario..</h1>
+
+    <form action="formulario/adicionar" method="post" class="form-horizontal">
+        {!! csrf_field() !!}
+
+        <div class="form-group">
+            <label for="nome" class="col-sm-2 control-label">Nome</label>
+            <div class="col-sm-10">
+                <input type="text" class="form-control" name="nome" id="nome" placeholder="Nome do cara">
+            </div>
+        </div>
+
+        <div class="form-group">
+            <label for="inputPassword3" class="col-sm-2 control-label">Password</label>
+            <div class="col-sm-10">
+                <input type="text" name="password" class="form-control" id="inputPassword3" placeholder="Password">
+            </div>
+        </div>
+        <div class="form-group">
+            <div class="col-sm-offset-2 col-sm-10">
+                <div class="checkbox">
+                    <label>
+                        <input type="checkbox"> Remember me
+                    </label>
+                </div>
+            </div>
+        </div>
+        <div class="form-group">
+            <div class="col-sm-offset-2 col-sm-10">
+                <button type="submit" class="btn btn-default">Sign in</button>
+            </div>
+        </div>
+    </form>
+
+@endsection
